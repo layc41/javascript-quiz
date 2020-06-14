52c3e09bee9c8949a45dee27956020346d0abf03
Claudia Lay <claudialay@claudias-mbp.home>
2020-06-14 13:31:06 -0500

    added questions and answers

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// questions
+var questions = [
+    new Question ("Commonly used data types DO not Include?", ["1. strings", "2. booleans", "3. alerts", "4. numbers"], "3. alerts"),
+    new Question ("The condition in an if / else statement is enclosed with ____.", ["1. quotes", "2. curly brackets", "3. parenthesis", "4. square brackets"], "3. parenthesis"),
+    new Question ("Arrays in JavaScript can be used to store ____.", ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"], "4. all of the above"),
+    new Question ("String values must be enclosed within _____ when being assigned to variables.", ["1. commas", "2. curly brackets", "3. quotes", "4. parenthesis"], "3. quotes"),
+    new Question ("A very useful tool used during development and debugging for printing content to the debugger is:", ["1. JavaScript", "2. terminal/bash", "3. for loops", "4. console log"], "3. alerts")
+]
